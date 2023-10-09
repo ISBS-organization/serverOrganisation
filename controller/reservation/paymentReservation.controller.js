6928579b3e3863c70a43cbde1e26a64a13e817bf
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-10-09 23:37:21 +0100

    create controller payment by id middleware

---
+++ controller/reservation/paymentReservation.controller.js
@@ -0,0 +1,29 @@
+  /*
+ /  ---- get reservations list
+/*/
+
+const Reservation = require("../../model/Reservation.model");
+
+
+const paymentReservation = async (req, res) => {
+  const {id} = req.body
+    // ----- get using information from request
+       try {
+    // ----- send email
+        const reservation = await Reservation.findByIdAndUpdate(
+          id,
+          {status: true},
+          { new: true }
+        )
+        console.log(reservation, "test")
+        if (reservation) {
+          return res.status(201).send({ message: "your reservation is payed with success", data: reservation})
+        }
+        return res.status(404).send({message: "id not find"})
+      } catch (error) {
+        console.log(error)
+        return res.status(404).send({message: "id not find"})
+      }
+    }
+
+    module.exports = paymentReservation
