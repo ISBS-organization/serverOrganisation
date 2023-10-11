c429bc314637347288c3cd0f0c2493147e2a19e1
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-10-11 03:20:30 +0100

    create generated code qr controller

---
+++ controller/codeQR/generateCodeQR.controller.js
@@ -0,0 +1,20 @@
+  /*
+ /  ---- generate codeQr code
+/*/
+
+const QRCode = require('qrcode')
+
+const generateCodeQr = async (req, res, next) => {
+    // ----- get using information from request
+    const test = Date.now()
+       try {
+        const codeQr = await QRCode.toFile( `asset/${test}.png`,`hello how are all nice to scan my Qr code `,{type:'png', errorCorrectionLevel: 'medium' })
+    // ----- response of the generated code qr
+       req.codeQr = codeQr
+       next()
+      } catch (error) {
+        console.log(error)
+        return res.status(500).send({message:'Internal server error'});
+      }
+    }
+module.exports = generateCodeQr
