fbbb5f2593c994eaaad08281697992a3fd6c800d
RaedRdhaounia <raedrdhaounia@gmail.com>
2023-10-12 02:04:11 +0100

    add regex details for search query

--- controller/getReservation/queryListSearch.middleware.js
+++ controller/getReservation/queryListSearch.middleware.js
@@ -1,32 +1,40 @@
-const  searchReservationQuery = (req, res, next)  => {
-  const { firstName, lastName, phoneNumber, email } = req.query;
+const user = require("../../model/user.model");
 
-  const query = {};
+const  searchReservationQuery = async(req, res, next)  => {
+  const { email, firstName, lastName, phoneNumber } = req.query;
 
-// Case-insensitive search for the firstName
+  try {
+  // Step 1: Search for users based on the query parameters
+  const userQuery = {};
+
+  if (email) {
+    userQuery.email = { $regex: new RegExp(email, 'i') }; // Case-insensitive partial match for email
+  }
+  
   if (firstName) {
-    query.firstName = { $regex: new RegExp(firstName, 'i') }; 
+    userQuery.firstName = { $regex: new RegExp(firstName, 'i') }; // Case-insensitive partial match for firstName
   }
-
-// Case-insensitive search for the lastName
+  
   if (lastName) {
-    query.lastName = { $regex: new RegExp(lastName, 'i') }; 
+    userQuery.lastName = { $regex: new RegExp(lastName, 'i') }; // Case-insensitive partial match for lastName
   }
-
-// Case-insensitive search for the email
-if (email) {
-    query.email = { $regex: new RegExp(email, 'i') }; 
+  
+  if (phoneNumber) {
+    userQuery.phoneNumber = { $regex: new RegExp(phoneNumber, 'i') }; // Case-insensitive partial match for phoneNumber
   }
-
-// Case-insensitive search for the phoneNumber
-if (phoneNumber) {
-    query.phoneNumber = { $regex: new RegExp(phoneNumber, 'i') }; 
-  }
-
 // Store the search query in the request object
-  req.searchQuery = query; 
+  req.userQuery = userQuery; 
 
+  const users = await user.find(userQuery).exec();
+  if (users.length === 0) {
+    return res.status(404).send({message: "no user find with this informations", data:[]})
+  }
+  req.usersId = users
   next();
+  } catch (error) {
+    return res.status(500).send({message: "server error please try later"})
+  }
+
 }
 
 module.exports = searchReservationQuery
